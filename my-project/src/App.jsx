bb4b6f853c84ecd9eba26687325e4a4c17fb3ea3
chase.haynes <chase.haynez@gmail.com>
2022-11-20 23:33:16 -0500

    updates
    forms

---
+++ my-project/src/App.jsx
@@ -0,0 +1,71 @@
+import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import Button from './Button';
+
+
+function App() {
+
+
+        //state //setState
+  const [route, setRoute] = useState('home');
+
+  
+    function changeRoute(){
+    if(route === 'home'){
+      setRoute('other')
+    }
+    if(route !== 'home'){
+      setRoute('home')
+    }
+    }
+
+
+  if(route === 'home'){
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         THIS IS HOME
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button handleClick={changeRoute} />
+      </header>
+     
+    </div>
+    )
+  }
+
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+       OTHER PAGE
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <Button handleClick={changeRoute}  />
+    </header>
+ 
+  </div>
+  );
+
+  
+}
+
+export default App;
